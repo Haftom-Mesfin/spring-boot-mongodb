32f24d5bff42c2b583cb54e772853e46d2bb968c
Haftom-Mesfin <sadtime1906@gmail.com>
2024-08-13 17:17:14 -0500

    Added and enabled Mongo Repository for  student collection

--- src/main/java/com/haftom/spring_boot_mongo/SpringBootMongoApplication.java
+++ src/main/java/com/haftom/spring_boot_mongo/SpringBootMongoApplication.java
@@ -2,8 +2,10 @@ package com.haftom.spring_boot_mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories("com.haftom.spring_boot_mongo.repository")
 public class SpringBootMongoApplication {
 
 	public static void main(String[] args) {
